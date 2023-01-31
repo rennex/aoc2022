ae292a12de2df8023fac73b9e57f2873aae74171
Renne Nissinen <rennex@iki.fi>
2023-02-01 00:47:03 +0200

    Improve day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,6 +2,7 @@
 // 23:30 start
 // 23:53 part1 done
 // 0:14 part2 done
+// 1:08 improved
 
 use input_downloader;
 
@@ -10,7 +11,7 @@ use std::collections::HashSet;
 
 fn main() {
     let input = input_downloader::year(2022).example("\
-vJrwpWtwJgWr hcsFMMfFFhFp
+vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
@@ -25,12 +26,11 @@ CrZsJsPPZsGzwwsLwLmpwMDw
         let items = parse_items(line);
         trio.push(items);
         if trio.len() == 3 {
-            let common_str = trio[0].intersection(&trio[1]).collect::<String>();
-            let common_perkele = parse_items(&common_str);
-            let common = common_perkele.intersection(&trio[2])
+            let common1: HashSet<char> = trio[0].intersection(&trio[1])
+                .copied().collect();
+            let common = common1.intersection(&trio[2])
                 .next().expect("no common item found!");
 
-            println!("Found badge: {common}");
             sum += priority_from_char(*common) as u32;
 
             trio.clear();
@@ -54,13 +54,4 @@ fn parse_items(line: &str) -> HashSet<char> {
         h.insert(c);
     }
     h
-}
-
-fn parse_line(line: &str) -> (HashSet<char>, HashSet<char>) {
-    let split_pos = line.len() / 2;
-    let compartment1 = &line[..split_pos];
-    let compartment2 = &line[split_pos..];
-    let items1 = parse_items(compartment1);
-    let items2 = parse_items(compartment2);
-    (items1, items2)
 }
